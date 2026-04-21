c1b60a8e7e642d8866369d611f99b96d526f4d12
YiruYang <162698398+yiruyang2025@users.noreply.github.com>
2026-04-21 23:31:38 +0200

    Update character.ts

--- app/types/character.ts
+++ app/types/character.ts
@@ -3,9 +3,8 @@ export interface Character {
   name: string | null;
   title: string | null;
   description: string | null;
-  isAlive: boolean;
-  actionPoints: number;
-  messageCount: number;    // how many messages already sent
-  messageSlots: number;    // total allowed messages, starts at 15
   secret: string | null;
+  isAlive: boolean;
+  messageCount: number;
+  actionPoints: number;
 }
