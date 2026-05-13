711deefd03dffb46bcabfd9e654cc2081ad297c2
zeyuwang-uzh <zeyu.wang2@uzh.ch>
2026-05-13 16:49:14 +0200

    refactor

--- app/hooks/useDirector.ts
+++ app/hooks/useDirector.ts
@@ -1,11 +1,11 @@
 import useLocalStorage from "@/hooks/useLocalStorage";
 
-export function useDirector(userId: number) {
+export function useDirector(id: number) {
   const { value: directorId, set: setDirectorId } = useLocalStorage<
     number | null
-  >(`director_scenarios_${userId}_id`, null);
+  >(`director_scenarios_${id}_id`, null);
   const { value: directorToken, set: setDirectorToken } = useLocalStorage<
     string | null
-  >(`director_scenarios_${userId}_token`, null);
+  >(`director_scenarios_${id}_token`, null);
   return { directorId, directorToken, setDirectorToken, setDirectorId };
 }
