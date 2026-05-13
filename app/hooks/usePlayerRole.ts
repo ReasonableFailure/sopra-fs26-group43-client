352070355513efb4906a2750ae2a7dfb832f8829
Felicia Faye Dinh <dinhfaye@protonmail.com>
2026-05-13 13:25:07 +0200

    UserId will not be null

--- app/hooks/usePlayerRole.ts
+++ app/hooks/usePlayerRole.ts
@@ -1,8 +1,8 @@
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { PlayerRole } from "@/types/playerRole";
 
-export const usePlayerRole = (userId: number | null) => {
-  const key = userId ? `playerRole_${userId}` : "playerRole_guest";
+export const usePlayerRole = (userId: number ) => {
+  const key = `playerRole_${userId}`;
   const { value: playerRole, set: setPlayerRole } = useLocalStorage<
     PlayerRole | null
   >(key, null);
