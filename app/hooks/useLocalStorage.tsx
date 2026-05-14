fa21191f5b119feeb4e0ea81b17a7525c2340f9e
Felicia Faye Dinh <dinhfaye@protonmail.com>
2026-05-14 13:06:25 +0200

    add debug-by-print statements

--- app/hooks/useLocalStorage.tsx
+++ app/hooks/useLocalStorage.tsx
@@ -20,8 +20,9 @@ export default function useLocalStorage<T>(
     try {
       const stored = globalThis.localStorage.getItem(key);
       if (stored !== null) setValue(JSON.parse(stored) as T);
+      console.log(JSON.parse(stored ?? "the thing is null"));
     } catch (error) {
-      console.error(`Error reading localStorage key "${key}":`, error);
+      console.log(`Error reading localStorage key "${key}":`, error);
     }
     setReady(true);
   }, [key]);
